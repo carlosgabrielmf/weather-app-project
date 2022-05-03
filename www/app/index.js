4e9c46c61412719ae19d47ddf7ad8901ac886896
carlosgabielmf <carlosg.mosquera12@gmail.com>
2022-05-03 16:51:34 +0200

    Successfully completed

--- www/app/index.js
+++ www/app/index.js
@@ -2,29 +2,29 @@ function getGeolocation() {
   const success = async (position) => {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
-    // latitude = 38.531;
-    // longitude = 172.024;
-    console.log(`Latitude is ${latitude}, and longitude is ${longitude}`);
+    // latitude = 44.355;
+    // longitude = 2.769;
+    // console.log(`Latitude is ${latitude}, and longitude is ${longitude}`);
     const API_KEY = '688feda454b3f179b097bbead2ef85e0';
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
     );
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
 
-    const current = data.current.weather[0].main;
-    console.log(current);
+    const daily = data.daily[0].weather[0].main;
+    // console.log(daily);
 
     const rain = document.querySelector('#rain');
     const noRain = document.querySelector('#noRain');
     if (
-      current === 'Rain' ||
-      current === 'Shower Rain' ||
-      current === 'Thunder Storm'
+      daily === 'Rain' ||
+      daily === 'Shower Rain' ||
+      daily === 'Thunder Storm'
     ) {
-      rain.style.visibility = 'visible';
+      rain.style.display = 'contents';
     } else {
-      noRain.style.visibility = 'visible';
+      noRain.style.display = 'contents';
     }
   };
   const error = (error) => {
